ad14491cec43d425310618fbe7b5e0d90dec0c8e
paddingme <huhaibo2012@gmail.com>
2016-04-27 01:26:33 +0800

    验证输入是否合法

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var app = express();
 var jwt = require('jsonwebtoken');
 var bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/index.html', express.static('index.html'))
 
@@ -19,6 +20,8 @@ app.post('/token', function(req, res) {
     var expiresIn = req.body.expiresIn;
 
     console.log(payload + '0000')
+    console.log(privateKey + '1111')
+    console.log(expiresIn + '22222')
     var token = jwt.sign(payload, privateKey, { expiresIn: expiresIn });
 
     console.log(token + 'token')
